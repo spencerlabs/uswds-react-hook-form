0ec51a616123a5329250ea3ba343d122181e484a
Morgan Spencer <morgan@spencercreative.co>
2022-12-15 15:35:05 -0500

    Configure repo for library mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,32 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/lib/index.ts'),
+      name: 'UswdsReactHookForm',
+      formats: ['es', 'umd'],
+      fileName: (format) => `uswds-react-hook-form.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom', 'react-hook-form'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          'react-hook-form': 'ReactHookForm',
+        },
+      },
+    },
+  },
 })
